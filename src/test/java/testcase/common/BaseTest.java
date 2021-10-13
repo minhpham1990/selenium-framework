6a9ffdc4afc16be4ec1d1dca6b8ad39356420e30
minhpham <phambaminh9990@gmail.com>
2021-10-13 22:39:45 +0700

    add take screenshot when fail

--- src/test/java/testcase/common/BaseTest.java
+++ src/test/java/testcase/common/BaseTest.java
@@ -1,11 +1,24 @@
 package testcase.common;
 
+import com.demo.constants.MyConstant;
 import com.demo.driver.DriverFactory;
 import com.demo.driver.DriverType;
+import com.demo.logger.MyLogger;
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class BaseTest {
@@ -29,6 +42,27 @@ public class BaseTest {
         });
     }
 
+    @AfterMethod
+    public void tearDownTestCase(ITestResult result){
+        if(result.getStatus()==ITestResult.FAILURE){
+            String methodName = result.getName();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyy_HHmmss");
+            Date date = new Date();
+            String currentDate = dateFormat.format(date);
+            String screenShotPath = MyConstant.PROJECT_PATH+"/screenshots/"+methodName+"-"+currentDate+".png";
+            try{
+                File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+                FileUtils.copyFile(srcFile,new File(screenShotPath));
+                Path path = Paths.get(screenShotPath);
+                try (InputStream is = Files.newInputStream(path)){
+                    Allure.addAttachment(methodName, is);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                MyLogger.logInfo("Fail to take screenshot");
+            }
+        }
+    }
     @AfterSuite(alwaysRun = true)
     public void quitDriver(){
         for(DriverFactory driver:webdriverThreadPool){
